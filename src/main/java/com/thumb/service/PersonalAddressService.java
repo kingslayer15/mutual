fe8d397cce725392e1902f91fd0574a412340a70
myyvonne <450455108@qq.com>
2019-06-08 16:21:18 +0800

    2019/6/8  收货地址

--- src/main/java/com/thumb/service/PersonalAddressService.java
+++ src/main/java/com/thumb/service/PersonalAddressService.java
@@ -3,9 +3,19 @@ package com.thumb.service;
 
 import com.thumb.dto.ClientAddressDto;
 
+import java.util.List;
+
 public interface PersonalAddressService {
 
     int newAddress(ClientAddressDto clientAddressDto);
 
-    ClientAddressDto getAaddressById(int uId);
+    List<ClientAddressDto> getAaddressById(int uId);
+
+    ClientAddressDto findOneAddress(ClientAddressDto clientAddressDto);
+
+    int updateOneAddress(ClientAddressDto clientAddressDto);
+
+    int deleteOneAddress(int id);
+
+    int setDefault(int id,int uid);
 }
--- src/main/java/com/thumb/service/impl/PersonalAddressServiceImpl.java
+++ src/main/java/com/thumb/service/impl/PersonalAddressServiceImpl.java
@@ -6,6 +6,9 @@ import com.thumb.service.PersonalAddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
+import java.util.List;
+
 @Service
 public class PersonalAddressServiceImpl implements PersonalAddressService {
     @Autowired
@@ -17,7 +20,29 @@ public class PersonalAddressServiceImpl implements PersonalAddressService {
     }
 
     @Override
-    public ClientAddressDto getAaddressById(int uId) {
+    public List<ClientAddressDto> getAaddressById(int uId) {
         return personalAddressMapper.getAaddressById(uId);
+    }
+
+    @Override
+    public ClientAddressDto findOneAddress(ClientAddressDto clientAddressDto) {
+        return personalAddressMapper.findOneAddress(clientAddressDto);
+    }
+
+    @Override
+    public int updateOneAddress(ClientAddressDto clientAddressDto) {
+        return personalAddressMapper.updateOneAddress(clientAddressDto);
+    }
+
+    @Override
+    public int deleteOneAddress(int id) {
+        return personalAddressMapper.deleteOneAddress(id);
+    }
+
+    @Override
+    public int setDefault(int id,int uid) {
+        //全设置为0 再更改默认地址
+        personalAddressMapper.allZero(uid);
+        return personalAddressMapper.setDefault(id);
     }
 }
