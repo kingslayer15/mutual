61fb56dc4edc22c563127f81243916cc59edfd23
Endanger25S <860822376@qq.com>
2019-06-12 19:53:53 +0800

    商家订单处理4

--- src/main/java/com/thumb/mapper/MerchantOmsOrderMapper.java
+++ src/main/java/com/thumb/mapper/MerchantOmsOrderMapper.java
@@ -21,4 +21,6 @@ public interface MerchantOmsOrderMapper {
     List<OmsOrderReturnApply> merchantOmsOrderReturnApply(int id);
 
     OmsOrderReturnApply merchantOmsOrderReturnApplyById(MerchantDto merchantDto);
+
+
 }
