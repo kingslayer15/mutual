6792d71f9fcb825e24d466f54de2860f8816b93b
lovinglifing <13929527132@163.com>
2019-06-14 23:20:51 +0800

    继续修改小样

--- src/main/java/com/thumb/interceptor/LoginInterceptor.java
+++ src/main/java/com/thumb/interceptor/LoginInterceptor.java
@@ -1,11 +1,13 @@
 package com.thumb.interceptor;
 
 
+import com.thumb.entity.pojo.UmsMember;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by DELL on 2019/5/27.
@@ -14,20 +16,22 @@ public class LoginInterceptor implements HandlerInterceptor{
     //前置
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
 
-//        String uri = httpServletRequest.getRequestURI();
-//        System.out.println(uri);
-//        //1.拦截除请求地址login、reg两个页面及其后续处理程序的所有请求
-//        //和filter不同，spring的拦截器只关注内容拦截
-//        if(uri.equals("/login.html")||uri.equals("/reg.html")){
-//            return true;
-//        }
-//        //2.如果有令牌，我就不拦截
-//        UserInfo userInfo = (UserInfo)httpServletRequest.getSession().getAttribute("userInfo");
-//        if(userInfo!=null){
-//            return true;
-//        }
-//        return false;
-        return true;
+
+        String uri = httpServletRequest.getRequestURI();
+
+        HttpSession session = httpServletRequest.getSession();
+
+        UmsMember umsMember = (UmsMember) session.getAttribute("umsMember");
+        Object admin = session.getAttribute("admin");
+        Object merchant = session.getAttribute("merchant");
+
+        if (umsMember != null || admin != null || merchant != null || uri.equals("/main/login.html")||uri.equals("/main/register.html")) {
+
+
+            return true;
+        }
+
+        return false;
     }
 
     //请求处理
